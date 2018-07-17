b42e1ae2f37e27de35932b2008fc6f8d5b53c746
Franky <liushuaigeq@163.com>
2018-07-17 15:25:28 +0800

    update

--- server/Tools/initServer.js
+++ server/Tools/initServer.js
@@ -1,3 +1,20 @@
-let async = require('async');
-let process = require('child_process');
+var async = require('async');
+var path = require('path');
+var process = require('child_process');
 
+var action = function () {
+    async.waterfall([
+        function (anext) {
+            //Gate Server 的初始化
+            process.exec('npm install', { cwd: path.join(__dirname, './GateServer/') }, function (err, stdout, stderr) {
+                console.log(err);
+                console.log(stdout);
+                console.log(stderr);
+                anext();
+            });
+        },
+    ], function (err) {
+        console.log('初始化完成');
+    });
+}
+action();
