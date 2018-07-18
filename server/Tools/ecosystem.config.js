84586fbfbb168ed296b9b0ef84db0ab1e583d20e
Franky <liushuaigeq@163.com>
2018-07-18 11:09:40 +0800

    update

--- server/Tools/ecosystem.config.js
+++ server/Tools/ecosystem.config.js
@@ -7,11 +7,19 @@ module.exports = {
             env: {
                 CONFIG_PATH: path.join(__dirname, '../Config/GateServer1/')
             }
-        }, {
+        },
+        {
             name: 'login1',
             script: path.join(__dirname, '../LoginServer/index.js'),
             env: {
                 CONFIG_PATH: path.join(__dirname, '../Config/LoginServer1/')
+            }
+        },
+        {
+            name: 'game1',
+            script: path.join(__dirname, '../GameServer/index.js'),
+            env: {
+                CONFIG_PATH: path.join(__dirname, '../Config/GameServer1/')
             }
         }
     ]
